5997079c1a711fb9629c9594aed984a4b54e9aa9
Aaron Dancer <me@aarondancer.com>
2015-05-21 15:42:54 -0500

    Moved towards usign hpanels for the actual layout

--- hpanel.js
+++ hpanel.js
@@ -1,5 +1,18 @@
 $(window).resize(function() {
-    $(".sph").each(function(){
-        $(this).css("min-height", $(this).parent().height());
-    });
+    sameAsParent();
 });
+
+$(window).load(function(){
+    sameAsParent();
+});
+
+function sameAsParent(){
+    $(".sph").each(function(){
+        $(this).css("min-height", '');
+    });
+    if ($(window).width() > 752) {
+        $(".sph").each(function(){
+            $(this).css("min-height", $(this).parent().height());
+        });
+    }
+}
